be6902f84db8e323c52279f7422f9345663b2b1a
Werner Reichmann <mail@wernerreichmann.net>
2020-02-28 20:21:14 +0100

    Update auswertung.R

--- auswertung.R
+++ auswertung.R
@@ -1,7 +1,7 @@
 library("dplyr")
 
 # Datensatz aus der csv-Datei importieren
-harzer_wasserstaende <- read.csv2("harzer_wasserstaende.csv")
+harzer_wasserstaende <- read.csv2("harzsperren_wasserstaende.csv")
 
 # Datensatz um Doubletten bereinigen
 harzer_wasserstaende_clean <- distinct(harzer_wasserstaende)
@@ -18,25 +18,25 @@ grane <- harzer_wasserstaende_clean[ which(harzer_wasserstaende_clean$talsperre=
 innerste <- harzer_wasserstaende_clean[ which(harzer_wasserstaende_clean$talsperre=="Innerste"), ]
 
 jpeg("oder_plot.jpg")
-plot(oder$datum, oder$stauinhaltfuellungsgrad, type="l", main = "Stauinhaltfüllungsgrad der Talsperre 'Oder'", xlab="Datum", ylab="Prozent (%)")
+plot(oder$datum, oder$stauinhaltfuellungsgrad, type="l", main = "Stauinhaltfüllungsgrad der Talsperre 'Oder'", xlab="Datum", ylab="Prozent (%)", ylim=c(0,100), col="blue", lwd=3)
 dev.off()
 
 jpeg("soese_plot.jpg")
-plot(soese$datum, soese$stauinhaltfuellungsgrad, type="l", main = "Stauinhaltfüllungsgrad der Talsperre 'Söse'", xlab="Datum", ylab="Prozent (%)")
+plot(soese$datum, soese$stauinhaltfuellungsgrad, type="l", main = "Stauinhaltfüllungsgrad der Talsperre 'Söse'", xlab="Datum", ylab="Prozent (%)", ylim=c(0,100), col="blue", lwd=3)
 dev.off()
 
 jpeg("ecker_plot.jpg")
-plot(ecker$datum, ecker$stauinhaltfuellungsgrad, type="l", main = "Stauinhaltfüllungsgrad der Talsperre 'Ecker'", xlab="Datum", ylab="Prozent (%)")
+plot(ecker$datum, ecker$stauinhaltfuellungsgrad, type="l", main = "Stauinhaltfüllungsgrad der Talsperre 'Ecker'", xlab="Datum", ylab="Prozent (%)", ylim=c(0,100), col="blue", lwd=3)
 dev.off()
 
 jpeg("oker_plot.jpg")
-plot(oker$datum, oker$stauinhaltfuellungsgrad, type="l", main = "Stauinhaltfüllungsgrad der Talsperre 'Oker'", xlab="Datum", ylab="Prozent (%)")
+plot(oker$datum, oker$stauinhaltfuellungsgrad, type="l", main = "Stauinhaltfüllungsgrad der Talsperre 'Oker'", xlab="Datum", ylab="Prozent (%)", ylim=c(0,100), col="blue", lwd=3)
 dev.off()
 
 jpeg("grane_plot.jpg")
-plot(grane$datum, grane$stauinhaltfuellungsgrad, type="l", main = "Stauinhaltfüllungsgrad der Talsperre 'Grane'", xlab="Datum", ylab="Prozent (%)")
+plot(grane$datum, grane$stauinhaltfuellungsgrad, type="l", main = "Stauinhaltfüllungsgrad der Talsperre 'Grane'", xlab="Datum", ylab="Prozent (%)", ylim=c(0,100), col="blue", lwd=3)
 dev.off()
 
 jpeg("innerste_plot.jpg")
-plot(innerste$datum, innerste$stauinhaltfuellungsgrad, type="l", main = "Stauinhaltfüllungsgrad der Talsperre 'Innerste'", xlab="Datum", ylab="Prozent (%)")
+plot(innerste$datum, innerste$stauinhaltfuellungsgrad, type="l", main = "Stauinhaltfüllungsgrad der Talsperre 'Innerste'", xlab="Datum", ylab="Prozent (%)", ylim=c(0,100), col="blue", lwd=3)
 dev.off()
